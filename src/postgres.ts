0dc9255cd6a72cd7a2a64ce9bc31d912804540f4
Ron Houben <r.houben@ksyos.nl>
2025-03-14 18:24:45 +0100

    first commit

---
+++ src/postgres.ts
@@ -0,0 +1,53 @@
+import pg from 'pg';
+import type { QueryResult, QueryResultRow, PoolConfig } from 'pg';
+
+export type PostgresClientOptions = Pick<PoolConfig, 'user' | 'password' | 'host' | 'port' | 'database'>;
+
+export class PostgresClient {
+  public readonly connectionString: string; // TODO: Don't expose this anymore
+  public readonly schemaPath = 'schema';
+
+  private readonly pool: pg.Pool;
+
+  constructor(options: PostgresClientOptions) {
+    this.connectionString = new URL(`postgres://${options.user}:${options.password}@${options.host}:${options.port}/${options.database}`).href;
+
+    this.pool = new pg.Pool({
+			user: options.user,
+			password: options.password,
+			host: options.host,
+			port: options.port,
+			database: options.database
+    });
+  }
+
+  public async query<T extends QueryResultRow>(
+    query: string,
+    params?: any[]
+  ): Promise<QueryResult<T>> {
+    const client = await this.pool.connect();
+
+    try {
+      return await client.query<T>(query, params);
+    } catch (e) {
+      const error = e as Error;
+
+      console.error('Query execution error:');
+      console.log({ error, query, params });
+
+      throw error;
+    } finally {
+			console.log('Rolling back transaction...');
+
+      await client
+        .query('ROLLBACK')
+        .catch((error) =>
+          console.warn('Could not roll back transaction:', error)
+        );
+
+			console.log('Releasing client...');
+      client.release();
+			console.log('Client released');
+    }
+  }
+}
---
+++ src/server.ts
@@ -0,0 +1,80 @@
+import { McpServer  } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { PostgresClient, PostgresClientOptions } from './postgres.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+import { z } from 'zod';
+
+type PostgresMcpServerOptions = {
+  mcp: {
+    name: string;
+    version: string;
+  };
+  database: PostgresClientOptions;
+};
+
+export class PostgresMcpServer {
+  public readonly postgres: PostgresClient;
+
+  private readonly server: McpServer;
+
+  constructor(options: PostgresMcpServerOptions) {
+    this.server = this.createPostgresMcpServer(options.mcp);
+    this.postgres = new PostgresClient({
+      user: options.database.user,
+      password: options.database.password,
+      host: options.database.host,
+      port: options.database.port,
+      database: options.database.database
+    });
+  }
+
+  public async start() {
+    console.log('Starting server...');
+
+    this.setTools();
+
+    const transport = new StdioServerTransport();
+
+    await this.server.connect(transport);
+
+    console.log('Server started successfully');
+  }
+
+  private createPostgresMcpServer(options: PostgresMcpServerOptions['mcp']) {
+    return new McpServer(
+      {
+        name: options.name,
+        version: options.version
+      },
+      {
+        capabilities: {
+          resources: {},
+          tools: {}
+        }
+      }
+    );
+  }
+
+  private setTools() {
+    console.log('Setting tools...');
+
+    this.setReadonlyQueryTool();
+
+    console.log('Tools set successfully');
+
+  }
+
+  private setReadonlyQueryTool() {
+    this.server.tool('readonly-query','Execute a read only query', { sqlQuery: z.string() }, async ({ sqlQuery }) => {
+        await this.postgres.query('BEGIN TRANSACTION READ ONLY');
+
+        const result = await this.postgres.query(sqlQuery);
+
+        return {
+          content: [
+            { type: 'text', text: JSON.stringify(result.rows, null, 2) }
+          ],
+        };
+      }
+    );
+  }
+}
