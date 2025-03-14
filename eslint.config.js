36305219e6687dece08f0a0b95cc84290354d3b4
Ron Houben <r.houben@ksyos.com>
2025-03-14 18:49:01 +0100

    feat: Add configuration files for ESLint and Prettier, and update PostgresClient code style

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tseslintParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslintParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/', 'coverage/'],
+  },
+  prettier,
+];
--- src/postgres.ts
+++ src/postgres.ts
@@ -1,7 +1,10 @@
 import pg from 'pg';
 import type { QueryResult, QueryResultRow, PoolConfig } from 'pg';
 
-export type PostgresClientOptions = Pick<PoolConfig, 'user' | 'password' | 'host' | 'port' | 'database'>;
+export type PostgresClientOptions = Pick<
+  PoolConfig,
+  'user' | 'password' | 'host' | 'port' | 'database'
+>;
 
 export class PostgresClient {
   public readonly connectionString: string; // TODO: Don't expose this anymore
@@ -10,20 +13,22 @@ export class PostgresClient {
   private readonly pool: pg.Pool;
 
   constructor(options: PostgresClientOptions) {
-    this.connectionString = new URL(`postgres://${options.user}:${options.password}@${options.host}:${options.port}/${options.database}`).href;
+    this.connectionString = new URL(
+      `postgres://${options.user}:${options.password}@${options.host}:${options.port}/${options.database}`
+    ).href;
 
     this.pool = new pg.Pool({
-			user: options.user,
-			password: options.password,
-			host: options.host,
-			port: options.port,
-			database: options.database
+      user: options.user,
+      password: options.password,
+      host: options.host,
+      port: options.port,
+      database: options.database,
     });
   }
 
   public async query<T extends QueryResultRow>(
     query: string,
-    params?: any[]
+    params?: unknown[]
   ): Promise<QueryResult<T>> {
     const client = await this.pool.connect();
 
@@ -37,17 +42,15 @@ export class PostgresClient {
 
       throw error;
     } finally {
-			console.log('Rolling back transaction...');
+      console.log('Rolling back transaction...');
 
       await client
         .query('ROLLBACK')
-        .catch((error) =>
-          console.warn('Could not roll back transaction:', error)
-        );
+        .catch((error) => console.warn('Could not roll back transaction:', error));
 
-			console.log('Releasing client...');
+      console.log('Releasing client...');
       client.release();
-			console.log('Client released');
+      console.log('Client released');
     }
   }
 }
